eae50c0976b6275436dd9f67e29938fa3d6cf618
Mikhaylovskiy M <wizmax@yandex.ru>
2015-08-08 21:31:31 +0300

    first commit

---
+++ PathKeeper/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace PathKeeper
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Требуется переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Обязательный метод для поддержки конструктора - не изменяйте
+		/// содержимое данного метода при помощи редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.FileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.Sync = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+			this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+			this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+			this.backgroundWorker3 = new System.ComponentModel.BackgroundWorker();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.listBox2 = new System.Windows.Forms.ListBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.menuStrip1.SuspendLayout();
+			this.statusStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// backgroundWorker1
+			// 
+			this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(804, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// FileToolStripMenuItem
+			// 
+			this.FileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Sync,
+            this.toolStripSeparator1,
+            this.выходToolStripMenuItem});
+			this.FileToolStripMenuItem.Name = "FileToolStripMenuItem";
+			this.FileToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+			this.FileToolStripMenuItem.Text = "Файл";
+			// 
+			// Sync
+			// 
+			this.Sync.Enabled = false;
+			this.Sync.Name = "Sync";
+			this.Sync.Size = new System.Drawing.Size(160, 22);
+			this.Sync.Text = "Синхронизация";
+			this.Sync.Click += new System.EventHandler(this.Sync_Click);
+			// 
+			// toolStripSeparator1
+			// 
+			this.toolStripSeparator1.Name = "toolStripSeparator1";
+			this.toolStripSeparator1.Size = new System.Drawing.Size(157, 6);
+			// 
+			// выходToolStripMenuItem
+			// 
+			this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+			this.выходToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+			this.выходToolStripMenuItem.Text = "Выход";
+			this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel1});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 302);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(804, 22);
+			this.statusStrip1.TabIndex = 1;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// toolStripProgressBar1
+			// 
+			this.toolStripProgressBar1.Enabled = false;
+			this.toolStripProgressBar1.MarqueeAnimationSpeed = 0;
+			this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+			this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+			this.toolStripProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+			// 
+			// toolStripStatusLabel1
+			// 
+			this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+			this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox1.Location = new System.Drawing.Point(0, 27);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(804, 27);
+			this.textBox1.TabIndex = 3;
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// backgroundWorker2
+			// 
+			this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+			// 
+			// backgroundWorker3
+			// 
+			this.backgroundWorker3.WorkerSupportsCancellation = true;
+			this.backgroundWorker3.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker3_DoWork);
+			// 
+			// listBox1
+			// 
+			this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+						| System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.listBox1.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 18;
+			this.listBox1.Location = new System.Drawing.Point(0, 58);
+			this.listBox1.Margin = new System.Windows.Forms.Padding(0);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(724, 238);
+			this.listBox1.TabIndex = 4;
+			this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+			// 
+			// listBox2
+			// 
+			this.listBox2.FormattingEnabled = true;
+			this.listBox2.Location = new System.Drawing.Point(58, 125);
+			this.listBox2.Name = "listBox2";
+			this.listBox2.Size = new System.Drawing.Size(565, 95);
+			this.listBox2.TabIndex = 5;
+			// 
+			// button2
+			// 
+			this.button2.Image = global::PathKeeper.Properties.Resources.onebit_12;
+			this.button2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+			this.button2.Location = new System.Drawing.Point(729, 145);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(63, 81);
+			this.button2.TabIndex = 7;
+			this.button2.Text = "Открыть папку";
+			this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button1
+			// 
+			this.button1.Image = global::PathKeeper.Properties.Resources.onebit_10;
+			this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+			this.button1.Location = new System.Drawing.Point(729, 58);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(63, 81);
+			this.button1.TabIndex = 6;
+			this.button1.Text = "Информация";
+			this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(804, 324);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.statusStrip1);
+			this.Controls.Add(this.menuStrip1);
+			this.Controls.Add(this.listBox2);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
+			this.Text = "Path Keeper";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.ComponentModel.BackgroundWorker backgroundWorker1;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem FileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem Sync;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+		private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.ComponentModel.BackgroundWorker backgroundWorker2;
+		private System.ComponentModel.BackgroundWorker backgroundWorker3;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.ListBox listBox2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+	}
+}
+
---
+++ PathKeeper/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PathKeeper
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		//переменные для хранения списка файлов и определения времени 
+		StringBuilder FileList = new StringBuilder();
+		TimeSpan ts;
+		Stopwatch stw;
+
+		//обработка глобального массива всех файлов
+		StringReader sr;
+
+		//выход
+		private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+
+		//рекурсивная функция для генерирования списка всех файлов на компьютере
+		internal int GetDirect(string dir)
+		{
+			try
+			{
+				//генерируем список директория и файлов из передаваемого в функцию пути
+				string[] SecondFLD = Directory.GetDirectories(dir);
+				string[] SecondFL = Directory.GetFiles(dir);
+
+				//добавляем в глобальный список файлы
+				foreach (string FL in SecondFL)
+				{
+					FileList.AppendLine(FL);
+				}
+
+				//переходим к следующей директории
+				foreach (string FLD in SecondFLD)
+				{
+					//добавляем папку
+					FileList.AppendLine(FLD);
+					GetDirect(FLD);
+				}
+			}
+			catch (System.Exception ex)
+			{
+
+			}
+
+			return 0;
+		}
+
+		//потом для обхода всех директория и генерации нового списка файлов
+		private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+		{
+			try
+			{
+				//списко дисков
+				string[] ALLDrive = Directory.GetLogicalDrives();
+				
+				foreach (string Drive in ALLDrive)
+				{
+					//список файлов в корне диска
+					string[] FL = Directory.GetFiles(Drive);
+					
+					foreach (string files in FL)
+					{
+						FileList.AppendLine(files);
+					}
+
+					//список директорий в корне диска
+					string[] FLD = Directory.GetDirectories(Drive);
+					foreach (string folder in FLD)
+					{
+						//добавляем папку
+						FileList.AppendLine(folder);
+						GetDirect(folder);
+					}
+
+				}
+			}
+			catch (System.Exception ex)
+			{
+				
+			}
+
+
+			//сохраняем список в файл
+
+			if(File.Exists("path.txt"))
+			{
+				File.Delete("path.txt");
+				File.WriteAllText("path.txt", FileList.ToString());
+			}
+			else
+			{
+				//создаем новый
+				File.WriteAllText("path.txt", FileList.ToString());
+			}
+
+			//время
+			ts = stw.Elapsed;
+			toolStripStatusLabel1.Text = "Завершено за " + ts.ToString();
+			toolStripProgressBar1.Visible = false;
+			Sync.Enabled = false;
+
+		}
+
+		//кнопка синхронизации
+		private void Sync_Click(object sender, EventArgs e)
+		{
+			toolStripProgressBar1.MarqueeAnimationSpeed = 10;
+			toolStripProgressBar1.Visible = true;
+			toolStripStatusLabel1.Text = "Идет обработка";
+
+			stw = Stopwatch.StartNew();
+			FileList.Remove(0, FileList.Length);
+			Sync.Enabled = false;
+			backgroundWorker1.RunWorkerAsync();
+
+		}
+
+		
+		private void Form1_Load(object sender, EventArgs e)
+		{		
+			CheckForIllegalCrossThreadCalls = false;
+			//загрузка данных
+			backgroundWorker2.RunWorkerAsync();
+		}
+		
+
+		//при загрузке формы загружаем в память список файлов
+		private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+		{
+
+			toolStripProgressBar1.MarqueeAnimationSpeed = 10;
+			toolStripStatusLabel1.Text = "Идет обработка";
+
+			try
+			{
+				//читаем из файла
+				FileList.Append(File.ReadAllText("path.txt", Encoding.UTF8));
+				toolStripStatusLabel1.Text = "Завершено";
+			}
+			catch (System.Exception ex)
+			{
+				toolStripStatusLabel1.Text = "Список файлов не обнаружен, сделайте синхронизацию";
+			}
+
+			toolStripProgressBar1.Visible = false;
+			Sync.Enabled = true;
+
+
+		}
+
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+			//при заполении поля поиск, очищаем итембокс
+			listBox1.Items.Clear();
+			listBox2.Items.Clear();
+
+			//если больше 3 символов тогда начинаем поиск
+			if (textBox1.Text.Length > 3)
+			{
+				toolStripProgressBar1.MarqueeAnimationSpeed = 10;
+				toolStripProgressBar1.Visible = true;
+				toolStripStatusLabel1.Text = "Идет обработка";
+
+				//если уже происходит поиск а пользователь продолжает набирать
+				//слово для поиска тогда, останавливаем  поиск и начинаем снова
+				try
+				{					
+					backgroundWorker3.CancelAsync();
+					backgroundWorker3.RunWorkerAsync();
+				}
+				catch (System.Exception ex)
+				{
+					backgroundWorker3.CancelAsync();
+				}
+
+			}
+
+		}
+
+		private void backgroundWorker3_DoWork(object sender, DoWorkEventArgs e)
+		{
+			//добавляем для обработки глобальный массив файлов
+			sr = new StringReader(FileList.ToString());
+
+			while (true)
+			{
+				try
+				{	//читаем построчно глобальный массив
+					string tmp = sr.ReadLine();
+
+					//выделяем последнее вхождение в строку слеша 
+					int h = tmp.LastIndexOf(@"\");
+
+					//вырезаем имя файла или папки
+					string FileName = tmp.Substring(h+1);
+
+					//если введенное для поиска слово содержиться в имени тогда добавляем его в результаты
+					if (FileName.IndexOf(textBox1.Text, StringComparison.OrdinalIgnoreCase) != -1)
+					{
+						//тогда добавляем его результаты поиска
+						//имя
+						listBox1.Items.Add(FileName);
+						//в зеркале полный путь
+						listBox2.Items.Add(tmp);
+					}
+
+				}
+				catch (System.Exception ex)
+				{
+					break;
+				}
+				
+			}
+
+			toolStripProgressBar1.Visible = false;
+			toolStripStatusLabel1.Text = "Завершено";
+		}
+
+		private void listBox1_DoubleClick(object sender, EventArgs e)
+		{
+			//при двойном клике по файлу открываем его в закрепленной к нему программе
+			int i = listBox1.SelectedIndex;
+			try
+			{
+				System.Diagnostics.Process.Start(listBox2.Items[i].ToString());
+			}
+			catch (System.Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+			
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			int SelectedItem = listBox1.SelectedIndex;
+			//глючное место
+			//я не знаю как увидеть различие между файлои и папкой
+			//сначала получаем инфу об объекте как о файле, если эксепшен
+			//тогда обрабатываем его как директорию, и то не всегда информация доступна
+			try
+			{	
+				//файл
+				FileInfo SelectedFileInfo = new FileInfo(listBox2.Items[SelectedItem].ToString());
+				string infoFile = "Имя файла:  " + SelectedFileInfo.Name +
+					"\r\nРазмер файла:  " + Convert.ToString(SelectedFileInfo.Length / 1024) + "Кб" +
+					"\r\nСоздан:  " + SelectedFileInfo.CreationTime.ToString() +
+					"\r\nАтрибуты:  " + SelectedFileInfo.Attributes.ToString();
+				MessageBox.Show(infoFile, "Информация o файлe", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+			}
+			catch (System.Exception ex)
+			{	
+				// :) папка
+				DirectoryInfo SelectedDirInfo = new DirectoryInfo(listBox2.Items[SelectedItem].ToString());
+				string infoDir = "Имя каталога:  " + SelectedDirInfo.Name +
+					"\r\nРодительский каталог:  " + SelectedDirInfo.Parent +
+					"\r\nСоздан:  " + SelectedDirInfo.CreationTime.ToString() +
+					"\r\nАтрибуты:  " + SelectedDirInfo.Attributes.ToString();
+				MessageBox.Show(infoDir, "Информация о каталоге", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+				
+			}
+
+
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+
+			try
+			{
+				//выделяем путь если выбран файл
+				int i = listBox1.SelectedIndex;
+
+				string path = listBox2.Items[i].ToString();
+
+				if (path.Length - path.LastIndexOf(".") < 5)
+				{
+					path = path.Remove(path.LastIndexOf(@"\"));
+				}
+
+				System.Diagnostics.Process.Start(path);
+			}
+			catch (System.Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+		
+	}
+}
